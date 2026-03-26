520d02d8f13043473cc3d0a03dd0de5f76bbeff3
Luciano Ramalho <luciano@ramalho.org>
2026-03-26 16:45:49 -0300

    prep para reformatar

---
+++ fluentpython2e/README.md
@@ -0,0 +1,5 @@
+# `lis.py` code as presented in Fluent Python 2nd edition
+
+Changes:
+
+* `ruff` formatted with option `quote-style = "single"`
--- fluentpython2e/lis.py
+++ fluentpython2e/lis.py
@@ -1,35 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 ################ Lispy: Scheme Interpreter in Python 3.10
+
 ## (c) Peter Norvig, 2010-18; See http://norvig.com/lispy.html
 ## Minor edits for Fluent Python, Second Edition (O'Reilly, 2021)
 ## by Luciano Ramalho, adding type hints and pattern matching.
 
-################ imports and types
 
+################ Imports and Types
+
+# tag::IMPORTS[]
 import math
 import operator as op
 from collections import ChainMap
-from collections.abc import Iterator
 from itertools import chain
-from typing import Any, TypeAlias
+from typing import Any, TypeAlias, NoReturn
 
 Symbol: TypeAlias = str
 Atom: TypeAlias = float | int | Symbol
 Expression: TypeAlias = Atom | list
+# end::IMPORTS[]
 
-################ parse, tokenize, and read_from_tokens
 
+################ Parsing: parse, tokenize, and read_from_tokens
 
 def parse(program: str) -> Expression:
     "Read a Scheme expression from a string."
     return read_from_tokens(tokenize(program))
 
-
 def tokenize(s: str) -> list[str]:
     "Convert a string into a list of tokens."
     return s.replace('(', ' ( ').replace(')', ' ) ').split()
-
 
 def read_from_tokens(tokens: list[str]) -> Expression:
     "Read an expression from a sequence of tokens."
@@ -47,7 +48,6 @@ def read_from_tokens(tokens: list[str]) -> Expression:
     else:
         return parse_atom(token)
 
-
 def parse_atom(token: str) -> Atom:
     "Numbers become numbers; every other token is a symbol."
     try:
@@ -59,9 +59,9 @@ def parse_atom(token: str) -> Atom:
             return Symbol(token)
 
 
+################ Global Environment
 
-################ global environment
-
+# tag::ENV_CLASS[]
 class Environment(ChainMap[Symbol, Any]):
     "A ChainMap that allows changing an item in-place."
 
@@ -72,7 +72,7 @@ class Environment(ChainMap[Symbol, Any]):
                 map[key] = value  # type: ignore[index]
                 return
         raise KeyError(key)
-
+# end::ENV_CLASS[]
 
 def standard_env() -> Environment:
     "An environment with some Scheme standard procedures."
@@ -116,17 +116,17 @@ def standard_env() -> Environment:
     return env
 
 
-################ interaction: a REPL
+################ Interaction: A REPL
 
-
-def repl(prompt: str = 'lis.py> ') -> None:
-    "A prompt-read-evaluate-print loop."
-    global_env: Environment = standard_env()
+# tag::REPL[]
+def repl(prompt: str = 'lis.py> ') -> NoReturn:
+    "A prompt-read-eval-print loop."
+    global_env = Environment({}, standard_env())
     while True:
-        val = evaluate(parse(input(prompt)), global_env)
+        ast = parse(input(prompt))
+        val = evaluate(ast, global_env)
         if val is not None:
             print(lispstr(val))
-
 
 def lispstr(exp: object) -> str:
     "Convert a Python object back into a Lisp-readable string."
@@ -134,105 +134,84 @@ def lispstr(exp: object) -> str:
         return '(' + ' '.join(map(lispstr, exp)) + ')'
     else:
         return str(exp)
+# end::REPL[]
 
 
-################ eval
+################ Evaluator
 
-KEYWORDS = ['quote', 'if', 'define', 'lambda']
+# tag::EVALUATE[]
+KEYWORDS = ['quote', 'if', 'lambda', 'define', 'set!']
 
 def evaluate(exp: Expression, env: Environment) -> Any:
     "Evaluate an expression in an environment."
     match exp:
-        case int(x) | float(x):                             # number literal
+        case int(x) | float(x):
             return x
-        case Symbol(var):                                   # variable reference
+        case Symbol(var):
             return env[var]
-        case ['quote', exp]:                                # (quote exp)
-            return exp
-        case ['if', test, consequence, alternative]:        # (if test consequence alternative)
+        case ['quote', x]:
+            return x
+        case ['if', test, consequence, alternative]:
             if evaluate(test, env):
                 return evaluate(consequence, env)
             else:
                 return evaluate(alternative, env)
-        case ['define', Symbol(var), value_exp]:            # (define var exp)
-            env[var] = evaluate(value_exp, env)
-        case ['define', [Symbol(name), *parms], *body       # (define (name parm...) body1 bodyN...)
-              ] if len(body) > 0:
-            env[name] = Procedure(parms, body, env)
-        case ['lambda', [*parms], *body] if len(body) > 0:  # (lambda (parm...) body1 bodyN...)
+        case ['lambda', [*parms], *body] if body:
             return Procedure(parms, body, env)
+        case ['define', Symbol(name), value_exp]:
+            env[name] = evaluate(value_exp, env)
+        case ['define', [Symbol(name), *parms], *body] if body:
+            env[name] = Procedure(parms, body, env)
         case ['set!', Symbol(name), value_exp]:
             env.change(name, evaluate(value_exp, env))
-        case [op, *args] if op not in KEYWORDS:             # (proc arg...)
-            proc = evaluate(op, env)
-            values = (evaluate(arg, env) for arg in args)
+        case [func_exp, *args] if func_exp not in KEYWORDS:
+            proc = evaluate(func_exp, env)
+            values = [evaluate(arg, env) for arg in args]
             return proc(*values)
         case _:
             raise SyntaxError(lispstr(exp))
+# end::EVALUATE[]
 
-
+# tag::PROCEDURE[]
 class Procedure:
     "A user-defined Scheme procedure."
 
-    def __init__(
-        self, parms: list[Symbol], body: list[Expression], env: Environment
+    def __init__(  # <1>
+        self,
+        parms: list[Symbol],
+        body: list[Expression],
+        env: Environment
     ):
-        self.parms = parms
+        self.parms = parms  # <2>
         self.body = body
         self.env = env
 
-    def __call__(self, *args: Expression) -> Any:
-        local_env = dict(zip(self.parms, args))
-        env: Environment = Environment(local_env, self.env)
-        result = None
-        for exp in self.body:
+    def __call__(self, *args: Expression) -> Any:  # <3>
+        local_env = dict(zip(self.parms, args))  # <4>
+        env = Environment(local_env, self.env)  # <5>
+        for exp in self.body:  # <6>
             result = evaluate(exp, env)
-        return result
+        return result  # <7>
+# end::PROCEDURE[]
 
 
+################ command-line interface
 
-################ non-interactive execution
-
-def run_lines(source: str) -> Iterator[Any]:
-    global_env: Environment = standard_env()
+def run(source: str) -> Any:
+    global_env = Environment({}, standard_env())
     tokens = tokenize(source)
     while tokens:
         exp = read_from_tokens(tokens)
-        yield evaluate(exp, global_env)
-
-
-def run(source: str) -> Any:
-    result = None
-    for result in run_lines(source):
-        pass
+        result = evaluate(exp, global_env)
     return result
 
-
-################ jupyter notebook integration
-
-from IPython.core.getipython import get_ipython
-
-if get_ipython() is None:
-    register_cell_magic = lambda f: f  # no-op
-else:
-    from IPython.core.magic import register_cell_magic
-
-@register_cell_magic
-def lispy(line, cell):
-    """Evaluate cell."""
-    return run(cell)
-
-
-################ main
-
-import sys
-
-def main():
-    code = sys.stdin.read()
-    if code.strip():
-        print(run(code))
+def main(args: list[str]) -> None:
+    if len(args) == 1:
+        with open(args[0]) as fp:
+            run(fp.read())
     else:
         repl()
 
 if __name__ == '__main__':
-    main()
+    import sys
+    main(sys.argv[1:])
